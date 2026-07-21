09e39d5616e9650ca51738f0012eba3b5a2c6200
AgungDH <agungdh@live.com>
2026-07-21 10:30:39 +0700

    init

---
+++ src/test/java/id/my/agungdh/rakawikchat/RakawikchatApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.rakawikchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RakawikchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
